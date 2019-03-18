c8929412d1dff26abce72fb2fadac99921256e88
NetaGee <37884723+NetaGee@users.noreply.github.com>
2019-03-18 14:03:52 -0500

    with buttons

--- total.js
+++ total.js
@@ -1,6 +1,6 @@
 // setting the height and width
 var padding = 100;
-var svg_w = 800;
+var svg_w = 1100;
 var svg_h = 700;
 
 var graph_w = svg_w - padding*2;
@@ -19,6 +19,19 @@ d3.json(("cpsgen.json"), function(error, data) {
     };
   });
 
+d3.select('button#occupation')
+  .on('click', function(d){
+    // const currentClass = d3.select('.box')
+    d3.select('.box')
+      .attr('class', 'box showplot1');
+  })
+
+d3.select('button#gender')
+  .on('click', function(d){
+    // const currentClass = d3.select('.box')
+    d3.select('.box')
+      .attr('class', 'box showplot2');
+  })
 function testFunction(data){
 console.log(data);
 console.log("What is happening?")
